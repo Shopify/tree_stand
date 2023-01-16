b41d1cabe6fc130c642ebd099200e77264a3b313
derekstride <derek.stride@shopify.com>
2023-01-16 11:13:28 -0500

    Add CI test to ensure documentation is complete

--- test/unit/version_test.rb
+++ test/unit/version_test.rb
@@ -4,4 +4,11 @@ class VersionTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::TreeStand::VERSION
   end
+
+  def test_everything_is_documented
+    report = `bundle exec yard stats`
+    documented = report.lines.detect { |line| line =~ /% documented/ }
+
+    assert_equal "100.00% documented", documented.strip
+  end
 end
